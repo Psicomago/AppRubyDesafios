fd0ad8ad37ae465b6dd18964cf0b2809f7dd9205
Carlos Aravena <carlosaravena.coach@gmail.com>
2022-07-08 13:31:36 -0400

    agregar metodos para habilidades no heredadas

--- evaluados/herencia/ejercicio2.rb
+++ evaluados/herencia/ejercicio2.rb
@@ -12,6 +12,7 @@ require_relative 'modulos'
 class Animal
     attr_reader :nombre
     def initialize(nombre)
+        raise ArgumentError, 'El atributo nombre debe ser de la clase string' if nombre.class != String
         @nombre = nombre
     end
     include Habilidades::Caminante
@@ -32,11 +33,23 @@ end
 class Pinguino < Ave
     include Alimentacion::Carnivoro
     include Habilidades::Nadador
+    def volar
+    'No soy Juan Salvador Gaviota, eh'
+    end
+    def aterrizar
+    'Ni siquiera he levantado el vuelo'
+    end
 end
 
 class Paloma < Ave
     include Habilidades::Volador
     include Alimentacion::Herbivoro
+    def nadar
+    'glup glup, me ahogo'
+    end
+    def sumergir
+    '¡Rrrrrrr, rrrrrr, glup glup Aggg!'
+    end
 end
 
 class Pato < Ave
@@ -49,20 +62,76 @@ end
 class Perro < Mamifero
     include Alimentacion::Carnivoro
     include Habilidades::Nadador
+    def volar
+        'guarr,grrr grrrr no puedo volar'
+    end
+    def aterrizar
+        'Ni siquiera he levantado el vuelo'
+    end
+
 end
 class Gato < Mamifero
     include Alimentacion::Carnivoro
+    def nadar
+        'miauuu, puedo nadar pero, ni los sueñes'
+    end
+    def sumergir
+        'no mames guey, soy un gato'
+    end
+    def volar
+        'no puedo volar, pero puedo trepar muy alto'
+    end
+    def aterrizar
+        'contorsión y derechito aterrizo. al saltar eso si'
+    end
+
 end
 class Vaca < Mamifero
     include Alimentacion::Herbivoro
+    include Habilidades::Nadador
+    def volar
+        'no puedo volar, pero puedo dar leche'
+    end
+    def aterrizar
+        'Ni siquiera he levantado el vuelo'
+    end
+
 end
 
 class Mosca < Insecto
     include Habilidades::Volador
+    def comer
+        'puedo comer heces, basura y animales en descomposición'
+    end
+    def nadar
+        'si fuera una mosca alcalina podría nadar'
+    end
+    def sumergir
+        'como soy una mosca alcalina peluda, me puedo sumergir '
+    end
 end
-class Maripoza < Insecto
+class Mariposa < Insecto
     include Habilidades::Volador
+    def comer
+        'como oruga puedo comer vegetales, como mariposa estoy a puro nectar'
+    end
+    def nadar
+        'la mayoría no podemos nadar'
+    end
+    def sumergir
+        'solo algunas mariposas nocturnas pueden sumergirse'
+    end
+
 end
 class Abeja < Insecto
     include Habilidades::Volador
+    def comer
+        'traigame una porcion de polen acompañado de un nectar, la jalea real es para mi reina'
+    end
+    def nadar
+        'puedo nadar maximo 10 minutos y para ponerme a salvo'
+    end
+    def sumergir
+        'glup glup agggggg'
+    end
 end
